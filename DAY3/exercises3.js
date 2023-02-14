565f0e81baba39ac422712b40e5d7057c05bbeda
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-14 16:30:01 -0300

    update

---
+++ DAY3/exercises3.js
@@ -0,0 +1,13 @@
+/* DAY 3 OF 100 DAYS OF CODE: JAVASCRIPT DAY 3
+
+-LEVEL 1 EXERCISES- */
+
+//1. Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+let firstName = '';
+let lastName = '';
+let country = '';
+let city = '';
+let age = '';
+let isMarried = '';
+let year = '';
+
