36510913154cfc4431b898582478920d4c07a377
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-15 01:52:28 -0300

    Update exercises3.js

--- DAY3/exercises3.js
+++ DAY3/exercises3.js
@@ -16,8 +16,26 @@ let firstName = 'Gautama',
     console.log(typeof(isMarried)); // Prints boolean
     console.log(typeof(city)); // Prints undefined
     console.log(typeof(year)); // Prints number
-    
 
+//EXERCISE 2 IS NOT FINISHED!!!
+//2. Check if type of '10' is equal to 10
+let num = '10'; // declare num equeal '10'
+console.log(typeof(num)); // check the type of '10'
+
+//EXERCISE 3 IS NOT FINISHED!!!
+//3. Check if parseInt('9.8') is equal to 10
+
+/* 4. Boolean value is either true or false.
+a) Write three JavaScript statement which provide truthy value. */
+console.log(3 > 2);//Prints true
+console.log(4 == 4);//Prints true
+console.log(2 >= 1);//Prints true
+//b) Write three JavaScript statement which provide falsy value.
+console.log(1 < 0);//Prints false
+console.log(6 <= 4);//Prints false
+console.log(1 !== 1);//Prints false
+
+//5. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
 
 
 
