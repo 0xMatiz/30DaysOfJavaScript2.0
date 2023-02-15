1fe3310540b33d961021238e729cf1cd29f8d09c
0xMatiz <82920805+Arkioze@users.noreply.github.com>
2023-02-15 01:21:41 -0300

    update

--- DAY3/exercises3.js
+++ DAY3/exercises3.js
@@ -3,11 +3,23 @@
 -LEVEL 1 EXERCISES- */
 
 //1. Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
-let firstName = '';
-let lastName = '';
-let country = '';
-let city = '';
-let age = '';
-let isMarried = '';
-let year = '';
+let firstName = 'Gautama',
+    lastName = 'Buda',
+    country = 'India',
+    city
+    age = null,
+    isMarried = false,
+    year = -368;
+
+    console.log(typeof(firstName)); // Prints string
+    console.log(typeof(age)); // Prints object
+    console.log(typeof(isMarried)); // Prints boolean
+    console.log(typeof(city)); // Prints undefined
+    console.log(typeof(year)); // Prints number
+    
+
+
+
+
+
 
